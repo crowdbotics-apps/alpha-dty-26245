f3020d10239e9a673efdff04123209bca0c11594
Crowdbotics <team@crowdbotics.com>
2021-05-06 10:58:56 +0000

    Update models

--- backend/alpha_dty_26245/wsgi.py
+++ backend/alpha_dty_26245/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alpha_dty_26245.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alpha_dty_26245.settings")
 
 application = get_wsgi_application()
